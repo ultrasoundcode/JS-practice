11f9fdb54eb8fef2666b862680f4abc626ab3ef5
Askhat Anarbayev <0x.ultrasound@gmail.com>
2022-12-29 13:07:44 +0600

    variables

---
+++ js/variables.js
@@ -0,0 +1,19 @@
+// variables
+var student = "Askhat"; // gthеменная var
+console.log(student); //Askhat
+
+let number = 35; // переменная let
+console.log(number); //35
+
+let age; // переменная let  можно обьявить без значения
+console.log(age); //undefined
+
+age = 35; //значение можно изменить позже
+console.log(age); //35
+age = 30;
+console.log(age); //30
+
+const userName = "Zhadyra"; //переменная const обьявляется сразу со значением
+console.log(userName); //Zhadyra
+let lastName = "Anarbayev";
+console.log(lastName);
