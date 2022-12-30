3fa0945e1776f591618a6ad544fc813867e16ea6
Askhat Anarbayev <0x.ultrasound@gmail.com>
2022-12-30 16:37:53 +0600

    toBoolean

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // import "./js/variables.js";
 // import "./js/primiteves.js";
 // import "./js/toString.js";
-
-import "./js/toNumber.js";
+// import "./js/toNumber.js";
+// import "./js/toBoolean.js";
+import "./js/false-values.js";
