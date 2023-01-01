9872a10a40fd3cd92110e4162f68af2afe201df3
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-01-01 13:58:44 +0600

    fin

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 // import "./js/toString.js";
 // import "./js/toNumber.js";
 // import "./js/toBoolean.js";
-import "./js/avtoCheck.js";
+// import "./js/avtoCheck.js";
 // import "./js/false-values.js";
