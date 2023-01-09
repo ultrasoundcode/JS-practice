15bdec5ec7505b685e28131ad8d32b3f5c26ae97
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-01-09 22:36:10 +0600

    1

--- js/avtocheck-array.js
+++ js/avtocheck-array.js
@@ -53,3 +53,18 @@ function checkStorage(available, ordered) {
   return message;
   // Change code above this line
 }
+//===   решение =====
+function checkStorage(available, ordered) {
+  if (ordered === 0) {
+    return 'Your order is empty!';
+  }
+
+  if (ordered > available) {
+    return 'Your order is too large, not enough goods in stock!';
+  }
+
+  return 'The order is accepted, our manager will contact you';
+
+  return message;
+}
+//================ Задача 4/32 ================
