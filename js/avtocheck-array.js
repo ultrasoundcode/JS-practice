16dfbf27b4f982f6e6e41927cdf0e02eb2479985
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-01-10 22:54:08 +0600

    autocheck

--- js/avtocheck-array.js
+++ js/avtocheck-array.js
@@ -38,33 +38,148 @@ ordered - количество единиц товара в заказе
 Вызов checkStorage(200, 250) возвращает "Your order is too large, not enough goods in stock!"
 Вызов checkStorage(150, 0) возвращает "Your order is empty!"
 */
-function checkStorage(available, ordered) {
-  // Change code below this line
-  let message;
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   let message;
 
-  if (ordered === 0) {
-    message = 'Your order is empty!';
-  } else if (ordered > available) {
-    message = 'Your order is too large, not enough goods in stock!';
-  } else {
-    message = 'The order is accepted, our manager will contact you';
-  }
+//   if (ordered === 0) {
+//     message = 'Your order is empty!';
+//   } else if (ordered > available) {
+//     message = 'Your order is too large, not enough goods in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
 
-  return message;
+//   return message;
+//   // Change code above this line
+// }
+// //===   решение =====
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+
+//   return 'The order is accepted, our manager will contact you';
+
+//   return message;
+// }
+//================ Задача 13/32 ================
+/*Термин slug - это человеко-понятный уникальный идентификатор,
+ который используется в веб-разработке для создания читабельных URL-адресов.
+Например, вместо того чтобы пользователь увидел в адресной строке mysite.com/posts/1q8fh74tx,
+ можно сделать slug из названия статьи. В результате адрес получится более приятным для восприятия: mysite.com/posts/arrays-for-begginers.
+Внимание
+Slug это всегда строка в нижнем регистре, слова которой разделены тире.
+
+Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title,
+ и возвращает slug, созданный из этой строки.
+
+Значением параметра title будут строки, слова которых разделены только пробелами
+Все символы slug должны быть в нижнем регистре
+Все слова slug должна быть разделены тире
+Объявлена функция slugify(title)
+Вызов slugify("Arrays for begginers") возвращает "arrays-for-begginers"
+Вызов slugify("English for developer") возвращает "english-for-developer"
+Вызов slugify("Ten secrets of JavaScript") возвращает "ten-secrets-of-javascript"
+Вызов slugify("How to become a JUNIOR developer in TWO WEEKS") возвращает "how-to-become-a-junior-developer-in-two-weeks"
+
+function slugify(title) {
+   // Change code below this line
   // Change code above this line
-}
-//===   решение =====
-function checkStorage(available, ordered) {
-  if (ordered === 0) {
-    return 'Your order is empty!';
+ }
+ */
+
+// function slugify(title) {
+//   let words = title.split(' ');
+//   for (let i = 0; i < words.length; i++) {
+//     words[i] = words[i].toLowerCase();
+//   }
+//   let slug = words.join('-');
+//   return slug;
+// }
+/*Данная функция сначала разделяет строку title на массив слов, используя пробел в качестве разделителя с помощью split().
+Затем циклом for перебирает массив слов, преобразует каждое слово в нижний регистр с помощью toLowerCase() и записывает результат обратно в массив.
+Затем функция использует join() чтобы объединить элементы массива в строку, используя тире в качестве разделителя. И возвращает результат.
+*/
+//================ Задача 14/32 ================
+
+/*Задача 14/32
+Дополни код так, чтобы переменные содержали частичные копии исходного массива fruits.
+
+firstTwoEls - массив из первых двух элементов
+nonExtremeEls - массив из всех элементов кроме первого и последнего
+lastThreeEls - массив из трёх последних элементов
+Объявлена переменная fruits
+Значение переменной fruits это массив ["apple", "plum", "pear", "orange", "banana"]
+Объявлена переменная firstTwoEls
+Значение переменной firstTwoEls это массив ["apple", "plum"]
+Объявлена переменная nonExtremeEls
+Значение переменной nonExtremeEls это массив ["plum", "pear", "orange"]
+Объявлена переменная lastThreeEls
+Значение переменной lastThreeEls это массив ["pear", "orange", "banana"]
+Переменной lastThreeEls присвоена копия части массива fruits после применения метода slice с правильными аргументами
+
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// Change code below this line
+const firstTwoEls = ;
+const nonExtremeEls = ;
+const lastThreeEls = ;
+*/
+//=решение=
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(2);
+//================ Задача 15/32 ================
+
+/*Дополни код так, чтобы в переменной allClients получился массив всех элементов массивов oldClients и newClients.
+
+Объявлена переменная oldClients
+Значение переменной oldClients это массив ["Mango", "Ajax", "Poly", "Kiwi"]
+Объявлена переменная newClients
+Значение переменной newClients это массив ["Peach", "Houston"]
+Объявлена переменная allClients
+Значение переменной allClients это массив ["Mango", "Ajax", "Poly", "Kiwi", "Peach", "Houston"]
+Переменной allClients присвоен массив после применения метода concat с правильными аргументами
+
+const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+const newClients = ['Peach', 'Houston'];
+
+const allClients = ; // Change this line
+*/
+//=решение=
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// const allClients = oldClients.concat(newClients); // Change this line
+//================ Задача 16/32 ================
+/* Напиши функцию makeArray(firstArray, secondArray, maxLength) для создания нового массива со всеми элементами двух исходных firstArray и secondArray.
+Параметр maxLength содержит максимально допустимую длину нового массива.
+
+Если количество элементов нового массива больше maxLength,
+функция должна вернуть копию массива длиной maxLength элементов.
+В противном случае функция должна вернуть новый массив целиком.
+
+function makeArray(firstArray, secondArray, maxLength) {
+    // Change code below this line
+
+
+
+    // Change code above this line
   }
-
-  if (ordered > available) {
-    return 'Your order is too large, not enough goods in stock!';
+*/
+//=решение=
+function makeArray(firstArray, secondArray, maxLength) {
+  let newArray = firstArray.concat(secondArray);
+  if (maxLength == 0) return [];
+  else if (newArray > maxLength) return newArray.slice(0, maxLength);
+  else {
+    return newArray;
   }
-
-  return 'The order is accepted, our manager will contact you';
-
-  return message;
 }
-//================ Задача 4/32 ================
